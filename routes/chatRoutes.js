c8832669fb8cb6275e3058789123f6819345d3ff
akash-cbl <akash.yadav@codebrewinnovations.com>
2024-12-13 17:10:00 +0530

    project intial

--- routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const chatController = require('../controllers/chatController');
-
-const router = express.Router();
-
-router.get('/', chatController.getHomePage);
-router.get('/chat/:id', chatController.getChatPage);
-
-module.exports = router;
